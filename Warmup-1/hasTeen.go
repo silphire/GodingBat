8e3c6e9981cd034c01dc0c75fedd39a0c5389857
Yuki Mitsui <silphire@gmail.com>
2019-02-16 09:07:27 +0900

    added hasTeen

---
+++ Warmup-1/hasTeen.go
@@ -0,0 +1,10 @@
+package main
+
+func hasTeen(nums ...int) bool {
+	for _, num := range nums {
+		if num >= 13 && num <= 19 {
+			return true
+		}
+	}
+	return false
+}
---
+++ Warmup-1/hasTeen_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestFirstIsTeen(t *testing.T) {
+	if !hasTeen(13, 10, 20) {
+		t.Fatal("first parameter has teen but testee says not so")
+	}
+}
+func TestSecondIsTeen(t *testing.T) {
+	if !hasTeen(21, 19, 113) {
+		t.Fatal("second parameter has teen but testee says not so")
+	}
+}
+func TestThirdIsTeen(t *testing.T) {
+	if !hasTeen(21, 29, 14) {
+		t.Fatal("second parameter has teen but testee says not so")
+	}
+}
+func TestNoOneIsTeen(t *testing.T) {
+	if hasTeen(21, 1, 113) {
+		t.Fatal("no parameter has teen but testee says not so")
+	}
+}
