3e97f919bbea6d55cc228ba575d0b582b754badb
Yuki Mitsui <silphire@gmail.com>
2019-06-26 08:28:53 +0900

    fixed test

--- warmup2/stringMatch_test.go
+++ warmup2/stringMatch_test.go
@@ -6,6 +6,6 @@ func TestStringMatch1(t *testing.T) {
 	expected := 3
 	actual := stringMatch("xxcaazz", "xxbaaz")
 	if expected != actual {
-		t.Fatal("expected %d, but actually %d", expected, actual)
+		t.Fatalf("expected %d, but actually %d", expected, actual)
 	}
 }
